86fb1183ef6c3685399b86da4de7fa329eb0e240
lin <wl1599@nyu.edu>
2018-02-09 17:14:41 -0500

    FastTextModel test

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import datetime as dt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn import preprocessing
 import pickle as pkl
 import random
 import os
+import gensim
 #from gensim.models import Word2Vec
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
@@ -104,7 +106,7 @@ def build_data(csv_file_name, company = None):
                 except:
                     pass
             waiting = set(tmp_waiting)
-
+    
     names = [t for t in set(df['author_id'].tolist()) if re.search('[a-zA-Z]', t)]
     # Drop these cases
     dialogues = [d for d in dialogues if len(set([t.author_id for t in d.tweets if t.author_id in names])) == 1]
@@ -136,7 +138,7 @@ def filter_and_split(dialogues):
             test.append(d.tweets[0].text)
         else:
             train.append(d.tweets[0].text)
-
+    
     f = open('data.pkl', 'wb')
     pkl.dump(data, f)
     f.close()
@@ -160,6 +162,9 @@ else:
     dialogues = build_data('twcs.csv', company = company_names)
     train, test, data = filter_and_split(dialogues)
 
+qa_map = dict()
+for d in data:
+    qa_map[d.tweets[0].text] = d.tweets[1].text
 
 
 stop_words = list(ENGLISH_STOP_WORDS) + ['flight', 'hey', 'delta', 'airline', 'airlines', 'american', 'air', 'americanair', 'british_airways', 'hi', 'plane', 'https']
@@ -175,7 +180,7 @@ pkl.dump(cv_model, f)
 f.close()
 
 # tfidf model is not consistent in threshold experiment
-tv = TfidfVectorizer(ngram_range = (1, 2), stop_words = 'english', max_features = 30000)
+tv = TfidfVectorizer(ngram_range = (1, 2), min_df = 2, max_features = 50000, stop_words = stop_words)
 vectors = tv.fit_transform(train).toarray()
 item_map = dict()
 for t in train:
@@ -197,17 +202,72 @@ f = open('FastTextEmbed.pkl', 'wb')
 pkl.dump(vec_map, f)
 f.close()
 
+def str2vec(string, embed_map):
+    '''
+    Convert a string to a row vector with a embedding map
+    '''
+    items = [item for item in string.lower().split() if item in embed_map]
+    matrix = np.array([embed_map[item] for item in items])
+    # Sequeeze the matrix to a row vector using averge pooling
+    return np.sum(matrix, axis = 0).reshape(1, -1)
+
 item_map = dict()
 vectors = np.zeros((len(train), 300)) 
 for i in range(len(train)):
     t = train[i]
     item_map[t] = i
-    vectors[i] = np.sum(np.array([vec_map[item] for item in t.split() if item in vec_map]), axis = 0).reshape(1, -1)
+    vectors[i] = str2vec(t, vec_map)
 
+vectors = preprocessing.normalize(vectors, norm = 'l2')
+ft_model = vector_model(item_map, vectors)
 
-qa_map = dict()
-for d in data:
-	qa_map[d.tweets[0].text] = d.tweets[1].text
+def test_embed_model(model, vec_map, outfile, test = test, topk = 2, qa_map = qa_map, threshold = 5):
+    filename = outfile if '.txt' in outfile else outfile + '.txt'
+    f = open(filename, 'w')
+    for idx in range(len(test)):
+        test_q = test[idx]
+        flag, match = model.get_nearest(str2vec(test_q, vec_map), topk = topk, threshold = threshold)
+        f.write('Test question: %d \n' % idx)
+        f.write('\n')
+        f.write(test_q + '\n')
+        f.write('\n')
+        f.write(qa_map[test_q] + '\n')
+        f.write('\n')
+        if flag:
+            f.write('**********SUCCESS**********\n')
+        else:
+            f.write('**********FAILURE**********\n')
+        f.write('\n')
+        for i in range(len(match)):
+            f.write('Top %d \n' % (i + 1))
+            f.write('\n')
+            f.write(match[i] + '\n')
+            f.write('\n')
+            f.write(qa_map[match[i]] + '\n')
+            f.write('\n')
+    f.close()
+
+def unit_embed_test(model, vec_map, test = test, topk = 2, qa_map = qa_map, threshold = 5):
+    for idx in range(len(test)):
+        test_q = test[idx]
+        can_vec = preprocessing.normalize(str2vec(test_q, vec_map), norm = 'l2')
+        flag, match = model.get_nearest(can_vec, threshold = threshold, topk = topk)
+        print('Test question %d \n' % idx)
+        print(test_q + '\n')
+        print(qa_map[test_q] + '\n')
+        score = model.vectors.dot(can_vec.T).flatten()
+        score.sort()
+        print(score[-topk:])
+        if flag:
+            print('**********SUCCESS**********\n')
+        else:
+            print('**********FAILURE**********\n')
+        for i in range(len(match)):
+            print('Top %d \n' % (i + 1))
+            print(match[i] + '\n')
+            print(qa_map[match[i]] + '\n')
+
+# tf_model performs pooly on test[5:15] 
 
 def test_model(model, vectorizer, outfile, test = test, topk = 2, qa_map = qa_map, threshold = 5):
     filename = outfile if '.txt' in outfile else outfile + '.txt'
@@ -235,7 +295,7 @@ def test_model(model, vectorizer, outfile, test = test, topk = 2, qa_map = qa_ma
             f.write('\n')
     f.close()
 
-def unit_test(model, vectorizer, test = test, topk = 3, qa_map = qa_map, threshold = 5):
+def unit_test(model, vectorizer, test = test, topk = 2, qa_map = qa_map, threshold = 5):
     for idx in range(len(test)):
         test_q = test[idx]
         flag, match = model.get_nearest(vectorizer.transform([test_q]).toarray(), threshold = threshold, topk = topk)
@@ -259,7 +319,7 @@ def test_threshold(vectors, v):
         cnt_train = cnt_train + 1 if flag else cnt_train
     for t in test:
         flag, _ = cv_model.get_nearest(cv.transform([t]).toarray(), topk = 2)
-
+    
     score = vectors.dot(v.T).flatten()
     score.sort()
     return score[-3:]
